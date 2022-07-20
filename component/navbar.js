ef86d3c045fe791612f2d333a632b800a07563af
rahulyadav826870 <rahulyadav826870@gmail.com>
2022-07-20 18:31:31 +0530

    Done first issue

---
+++ component/navbar.js
@@ -0,0 +1,78 @@
+let homeNavbar=()=>{
+     return ` <div>
+     <a href="index.html">NDTV</a>
+     <a href="">हिंदी न्यूज़</a>
+     <a href="">Bussiness</a>
+     <a href="">Movies</a>
+     <a href="">Cricket</a>
+     <a href="">Tech</a>
+     <a href="">Food</a>
+     <a href="">Crypto</a>
+     <a href="">Web Stories</a>
+     <a href="">Education</a>
+     <a href="">Auto</a>
+     <a href="">Swasth</a>
+     <a href="">Lifestyle</a>
+     <a href="">Health</a>
+     <a href="">Shopping</a>
+     <a href="">Art</a>
+ </div>
+ <div>
+     <a href="index.html"><img src="https://drop.ndtv.com/homepage/images/ndtvlogo23march.png" alt="NDTV logo" /></a>
+     <a href="liveTv.html">Live tv</a>
+     <a href="ndtvLatest.html">latest</a>
+     <a href="">treading</a>
+     <a href="">india</a>
+     <a href="">opinion</a>
+     <a href="">weather</a>
+     <a href="">cities</a>
+     <a href="">world</a>
+     <a href="">offbeat</a>
+     <a href="">stock market</a>
+     <a href="">photos</a>
+     <a  href=""><i class="fa-brands fa-twitter"></i>
+     </a>
+     <a href="userLogin.html"><i class="fa-brands fa-facebook-square"></i></a>
+     <a href=""><i class="fa-solid fa-bell"></i></a>
+     <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+ </div>`
+}
+
+let particularNavbar=()=>{
+     return`<div id="navbar">
+     <div>
+         <a href="">NDTV</a>
+         <a href="">हिंदी न्यूज़</a>
+         <a href="">Bussiness</a>
+         <a href="">Movies</a>
+         <a href="">Cricket</a>
+         <a href="">Tech</a>
+         <a href="">Food</a>
+         <a href="">Crypto</a>
+         <a href="">Web Stories</a>
+         <a href="">Education</a>
+         <a href="">Auto</a>
+         <a href="">Swasth</a>
+         <a href="/lifestyle.html">Lifestyle</a>
+         <a href="">Health</a>
+         <a href="">Shopping</a>
+         <a href="">Art</a>
+     </div>
+     <div>
+         <a href="index.html"><img src="https://drop.ndtv.com/homepage/images/ndtvlogo23march.png" alt="NDTV logo" /></a>
+         <a href="liveTv.html">Live tv</a>
+         <a href="ndtvLatest.html">latest</a>
+         <a href="">treading</a>
+         <a href="">india</a>
+         <a href="">opinion</a>
+         <a href="">weather</a>
+         <a href="">cities</a>
+         <a href="/world.html">world</a>
+         <a href="/offbeat.html">offbeat</a>
+         <a href="">stock market</a>
+         <a href="image.html">photos</a>
+         <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+     </div>`
+}
+
+export {homeNavbar,particularNavbar};
