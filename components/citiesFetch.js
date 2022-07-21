00a55002efcaa02ab2efddc8f4c270d16026764c
rahulyadav826870 <103634544+rahulyadav826870@users.noreply.github.com>
2022-07-22 03:49:01 +0530

    Add files via upload

---
+++ components/citiesFetch.js
@@ -0,0 +1,69 @@
+let getData=async(url)=>{
+     try{
+          let res= await fetch(url)
+               let data = await res.json()
+                  return data
+     }catch(err){
+          console.log("Something wrong, please check your code")
+     }
+      }
+
+     let displayTreadingNews =(data2,container,className,displayProperty)=>{
+          container.innerHTML=null
+
+          let p=document.createElement("p")
+          p.innerText="Treading"
+          p.style.display=`${displayProperty}`
+
+
+          container.append(p)
+
+          data2.forEach(({image_url,headline})=> {
+            let div = document.createElement("div");
+            div.setAttribute("class", `${className}`)
+        
+            let img = document.createElement("img");
+            img.src=image_url;
+        
+            let p = document.createElement("p");
+            p.innerText =headline;
+        
+            div.append(img, p);
+            container.append(div);
+            
+          });
+        }
+
+       let displaymidNews=(data,container)=> {
+          container.innerHTML=null
+          data.forEach(({image_url,headline,posted_date,description})=> {
+            let div = document.createElement("div");
+     
+            let div1 = document.createElement("div");
+
+            let img = document.createElement("img");
+            img.src=image_url;
+       
+            let div2 = document.createElement("div");
+
+            let h2 = document.createElement("h2");
+            h2.innerText = headline;
+
+            let p1 = document.createElement("p");
+            p1.innerText = posted_date;
+
+            let p2 = document.createElement("p");
+            p2.innerText =description;
+        
+            div1.setAttribute("class", "image");
+            div2.setAttribute("class", "innertext");
+
+            div.append(div1, div2);
+            div1.append(img);
+            div2.append(h2, p1, p2);
+
+            container.append(div);
+          });
+        }
+
+      export {getData,displayTreadingNews,displaymidNews};
